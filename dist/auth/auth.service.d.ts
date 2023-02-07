8606609adb00f064cda74aa01d82dc2e1ec26eed
trandai2401 <trandai@gmail.com>
2023-02-07 22:30:21 +0700

    add login

---
+++ dist/auth/auth.service.d.ts
@@ -0,0 +1,13 @@
+import { UsersService } from 'src/users/users.service';
+import { CreateAuthDto } from './dto/create-auth.dto';
+import { UpdateAuthDto } from './dto/update-auth.dto';
+export declare class AuthService {
+    private usersService;
+    constructor(usersService: UsersService);
+    validateUser(username: string, pass: string): Promise<any>;
+    create(createAuthDto: CreateAuthDto): string;
+    findAll(): string;
+    findOne(id: number): string;
+    update(id: number, updateAuthDto: UpdateAuthDto): string;
+    remove(id: number): string;
+}
